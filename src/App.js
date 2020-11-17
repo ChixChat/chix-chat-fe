0197a77c00291dcd128cd32b0a37d9a5c727d3cb
“Lauren <lucerolaurenm@gmail.com>
2020-11-17 14:11:08 -0500

    Setup ThemeProvider and CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
 const App = () => (
-  <div>
-    <p>A bare bones application!</p>
-  </div>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <div>
+      <p>A bare bones application!!!</p>
+    </div>
+  </ThemeProvider>
 )
 export default App
